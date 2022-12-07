09863daaf7346a66c439f55deba8f120bfafb226
Viktor Strelianyi <chmpain@gmail.com>
2022-12-07 02:04:42 +0200

    - update

--- src/app/swiper-old/swiper-old.component.ts
+++ src/app/swiper-old/swiper-old.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
 
 import SwiperCore from 'swiper/core';
 
 @Component({
   selector: 'app-swiper-old',
   templateUrl: './swiper-old.component.html',
-  styleUrls: ['./swiper-old.component.scss']
+  styleUrls: ['./swiper-old.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SwiperOldComponent implements OnInit {
 
@@ -14,11 +15,31 @@ export class SwiperOldComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public swiperConfig = {
+    slidesPerView: 1,
+    spaceBetween: 38,
+    navigation: false,
+    breakpoints:{
+      992:{
+        slidesPerView: 5,
+      }
+    }
+  }
+
   onSwiper(swiper:any) {
     console.log(swiper);
   }
   onSlideChange() {
     console.log('slide change');
   }
+
+  @ViewChild( 'swiper', { static: false }) swiper?: SwiperOldComponent;
+
+  // slideNext(){
+  //   this?.swiper?.swiperRef.slideNext(300);
+  // }
+  // slidePrev(){
+  //   this?.swiper?.swiperRef.slidePrev(300);
+  // }
 
 }
